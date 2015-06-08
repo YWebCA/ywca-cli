425e4cf16bcde2ba760bfe53cdb310a9ed1add5a
Nathan Stier <nathan.stier@gmail.com>
2015-06-08 14:04:26 -0500

    add help support

--- index.js
+++ index.js
@@ -14,24 +14,42 @@ var types = [
 var response = "If this was working, we would have just "
 
 var tasks = {
-  new: function(target) {
-    console.log(response + "generated a workspace in directory " + target + ".")
+  new: {
+    runner: function(target) {
+      console.log(response + "generated a workspace in directory " + target + ".")
+    },
+    help: "new"
   },
-  install: function(target) {
-    console.log(response + "installed the module ywca-xxxx-xxxx-" + target + ".")
+  install: {
+    runner: function(target) {
+      console.log(response + "installed the module ywca-xxxx-xxxx-" + target + ".")
+    },
+    help: "install"
   },
-  search: function(target) {
-    response += "searched npm for "
-    for (concern of concerns)
-      for (type of types)
-        response += "ywca-" + concern + "-" + type + "-" + target + ", "
-    response = response.substr(0, response.length - 2)
-    response = response.substr(0, response.lastIndexOf(",") + 1) + " and" + response.substr(response.lastIndexOf(",") + 1) + "."
-    console.log(response)
+  search: {
+    runner: function(target) {
+      response += "searched npm for "
+      for (concern of concerns)
+        for (type of types)
+          response += "ywca-" + concern + "-" + type + "-" + target + ", "
+      response = response.substr(0, response.length - 2)
+      response = response.substr(0, response.lastIndexOf(",") + 1) + " and" + response.substr(response.lastIndexOf(",") + 1) + "."
+      console.log(response)
+    },
+    help: "search"
+  },
+  help: {
+    runner: function(target) {
+      if (tasks[target])
+        console.log(tasks[target].help)
+      else
+        console.log("No help documentation exists for " + target + ".")
+    },
+    help: "help"
   }
 }
 
 if (tasks[command])
-  tasks[command](target)
+  tasks[command].runner(target)
 else 
 	console.log("No task was found for command [" + command + "] and target [" + target + "]!")
