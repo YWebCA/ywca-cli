34e1da0ac1a78d66b631e4db71a296ec15755007
Nathan Stier <nathan.stier@gmail.com>
2015-06-09 09:48:39 -0500

    Move cfg loading to where it is needed: search
    Signed-off-by: Nathan Stier <nathan.stier@gmail.com>

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
 var command = process.argv[2]
 var target = process.argv[3]
 
-var concerns = require('./cfg/concerns.json')
-var types = require('./cfg/types.json')
-
-var tasks = {
-  new: {
-    runner: require('./tasks/new')
-  },
-  install: {
-    runner: require('./tasks/install')
-  },
-  search: {
-    runner: require('./tasks/search')
-  },
-  help: {
-    runner: require('./tasks/help')
-  }
-}
-
-if (tasks[command])
-  tasks[command].runner(target)
-else 
+try {
+  require('./tasks/' + command)(target)
+} catch (e) {
 	console.log("No task was found for command [" + command + "] and target [" + target + "]!")
+}
--- tasks/search/index.js
+++ tasks/search/index.js
@@ -1,3 +1,6 @@
+var concerns = require('./cfg/concerns.json')
+var types = require('./cfg/types.json')
+
 module.exports = function(target) {
   response += "If this was working, we would have just searched npm for "
   for (concern of concerns)
