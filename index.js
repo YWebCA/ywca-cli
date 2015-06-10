a2e9b508d831435c66f83ca44ff9bec711aac2d8
Nathan Stier <nathan.stier@gmail.com>
2015-06-10 16:18:42 -0500

    Add env line to run from bin

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+#! /usr/bin/env node
+
 var command = process.argv[2]
 var target = process.argv[3]
 
 try {
   require('./tasks/' + command)(target)
 } catch (e) {
-	console.log("No task was found for command [" + command + "] and target [" + target + "]!")
+  console.log("No task was found for command [" + command + "] and target [" + target + "]!")
 }
