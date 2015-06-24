02f1da191c1e3ff9774d1ef9c1bb3c2244851dff
Nathan Stier <nathan.stier@gmail.com>
2015-06-23 23:31:16 -0500

    impliment install

--- tasks/install/index.js
+++ tasks/install/index.js
@@ -1,3 +1,3 @@
 module.exports = function(target) {
-  console.log("If this was working, we would have just installed the module ywca-xxxx-xxxx-" + target + ".")
+  global.env.run('ywca:' + target)
 }
