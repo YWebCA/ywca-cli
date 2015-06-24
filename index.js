7cdb7d9f46f9f3757716d33dff9e0610f83114db
Nathan Stier <nathan.stier@gmail.com>
2015-06-23 23:33:55 -0500

    register generators from node_modules

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 #! /usr/bin/env node
-
+var async = require('async')
+var fs = require('fs')
+var path = require('path')
 var yeoman = require('yeoman-environment')
 global.env = yeoman.createEnv()
 
 var command = process.argv[2]
 var target = process.argv[3]
 
+var registers = []
+
+function getDirectories(srcpath) {
+  
+  return fs.readdirSync(srcpath).filter(function(file) {
+    return fs.statSync(path.join(srcpath, file)).isDirectory()
+  });
+}
+
 env.register(__dirname + '/generators/app/index.js', 'ywca:app')
+for (module of getDirectories(process.cwd() + '/node_modules/')) {
+  if (module.indexOf('ywca-') === 0) {
+    env.register(process.cwd() + "/node_modules/" + module + '/generators/app/index.js', module.replace('-', ':'))
+  }
+}
 
 try {
   require('./tasks/' + command)(target)
